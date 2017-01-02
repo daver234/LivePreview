b14fcbbf6841183eaec054ae0c37243312b286b3
daver234 <dave.rothschild@gmail.com>
2017-01-01 22:22:19 -0800

    Initial Commit

---
+++ LivePreview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LivePreview
+//
+//  Created by David Rothschild on 1/1/17.
+//  Copyright © 2017 Dave Rothschild. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
